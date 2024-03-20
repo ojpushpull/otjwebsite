cd358a4c6e72aa8b2568fcd605b42ea225dd349b
Okera Johnson <okerajohnson@gmail.com>
2024-03-20 14:49:54 -0400

    new site for the new me

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import SimpleContainer from './components/SimpleContainer';
+import ColumnsGrid from './components/Grid';
+
 
 function App() {
   return (
+    
     <div className="App">
+      <SimpleContainer> HELLO</SimpleContainer>
+      <ColumnsGrid>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +24,7 @@ function App() {
           Learn React
         </a>
       </header>
+      </ColumnsGrid>
     </div>
   );
 }
