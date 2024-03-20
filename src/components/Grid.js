d87fbe14b8b87348c11759066a330571e49ae4a7
Okera Johnson <okerajohnson@gmail.com>
2024-03-20 15:33:39 -0400

    finish layda

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,6 +3,7 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import Types from './Text';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -17,7 +18,7 @@ export default function ColumnsGrid() {
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2} columns={2}>
         <Grid item xs={1}>
-          <Item>Summary</Item>
+          <Item><Types></Types></Item>
         </Grid>
         <Grid item xs={1}>
           <Item>Information</Item>
---
+++ src/components/Text.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+export default function Types() {
+  return (
+    <Box sx={{ width: '100%', maxWidth: 500 }}>
+      
+      <Typography variant="body1" gutterBottom>
+        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+        neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+        quasi quidem quibusdam.
+      </Typography>
+     
+      
+    </Box>
+  );
+}
